1dd86fbffd8e9ec97c3df9687b9f73936c7a4a3c
Cooper <ccubed.techno@gmail.com>
2015-12-07 05:28:23 +0000

    add cmdset

---
+++ FSuns/commands/fsunsset.py
@@ -0,0 +1,13 @@
+"""
+Fsunsset
+
+Fading Suns command set
+
+"""
+
+from evennia import CmdSet
+from commands import fsunscmds
+
+class FSunSet(CmdSet):
+    def at_cmdset_creation(self):
+        self.add(fsunscmds.Sheet)
