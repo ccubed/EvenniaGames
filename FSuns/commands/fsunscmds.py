dc9f2d1f67ee8cbff85dc3f3d78908ad2bdfe097
Cooper <ccubed.techno@gmail.com>
2015-12-09 01:21:42 +0000

    fixing

--- FSuns/commands/fsunscmds.py
+++ FSuns/commands/fsunscmds.py
@@ -38,13 +38,21 @@ class Sheet(default_cmds.MuxCommand):
         table.add_row("Endurance: " + str(self.caller.db.attributes['Endurance']), "Tech: " + str(self.caller.db.attributes['Tech']), "Faith: " + str(self.caller.db.attributes['Faith']))
         self.caller.msg(table)
         self.caller.msg(pad("Skills", width=80, align="c", fillchar="="))
-        for x in self.caller.db.skills.keys():
-            data.append(x + ': ' + str(self.caller.db.skills[x]))
-        table = evtable.EvTable(header=False)
+        data = {}
         i = 0
-        while i < len(data):
-            table.add_row(data[i],data[i+1],data[i+2])
-            i+=2
+        table = evtable.EvTable(header=False,align="c",width="80")
+        for x in self.caller.db.skills.keys():
+            data[i]=x + ': ' + str(self.caller.db.skills[x]))
+            if i == 2:
+                table.add_row(data[0], data[1], data[2])
+                i = 0
+            else:
+                i += 1
+        if len(data) > 0:
+            if len(data) == 1:
+                table.add_row(data[0])
+            else:
+                table.add_row(data[0], data[1])
         self.caller.msg(unicode(table))
         if len(self.caller.db.occult):
             self.caller.msg(pad("Occult", width=80, align="c", fillchar="="))
@@ -55,20 +63,22 @@ class Sheet(default_cmds.MuxCommand):
             for x in self.caller.db.actions.keys():
                 self.caller.msg(wrap(x + ": " + self.caller.db.actions[x], width=80, indent=3))
         if len(self.caller.db.languages):
-            i = 0
-            a = 0
-            data = {}
             self.caller.msg(pad("Languages", width=80, align="c", fillchar="="))
-            for x,v in self.caller.db.skills.values():
-                data[a] = {i: x + ': ' + v}
+            data = {}
+            i = 0
+            table = evtable.EvTable(header=False,align="c",width="80")
+            for x in self.caller.db.languages:
+                data[i]=x
                 if i == 2:
-                    i=0
-                    a+=1
+                    table.add_row(data[0], data[1], data[2])
+                    i = 0
                 else:
                     i += 1
-            table = evtable.EvTable(header=False)
-            for x in data.keys():
-                table.add_row(data[x][0],data[x][1],data[x][2])
+            if len(data) > 0:
+                if len(data) == 1:
+                    table.add_row(data[0])
+                else:
+                    table.add_row(data[0], data[1])
             self.caller.msg(unicode(table))
             
             
