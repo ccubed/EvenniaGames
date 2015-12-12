6192169f1dfd50a2bea9a884b1d348313dc7c9e6
Cooper <ccubed.techno@gmail.com>
2015-12-12 03:13:06 +0000

    Test

--- FSuns/commands/fsunscmds.py
+++ FSuns/commands/fsunscmds.py
@@ -27,65 +27,62 @@ class Sheet(default_cmds.MuxCommand):
     help_category = "Characters"
     
     def func(self):
-        if self.caller.db.house == 'None':
-            self.caller.msg('Go through CG first. Type +cg.')
-        else:
-            self.caller.msg(pad("Demographics", width=80))
-            self.caller.msg(pad("Name: " + self.caller.key, width=40, align="l") + pad("Archetype: " + self.caller.db.archetype, width=40, align="r"))
-            self.caller.msg(pad("Faction: " + self.caller.db.house, width=40, align="l") + pad("Rank: " + self.caller.db.benefices['Rank'], width=40, align="r"))
-            self.caller.msg(pad("Firebirds: " + str(self.caller.db.firebirds), width=40, align="l") + pad("Assets: " + str(self.caller.db.assets), width=40, align="r"))
-            self.caller.msg(pad("Attributes", width=80, align="c", fillchar="="))
-            table = evtable.EvTable("Body", "Mind", "Spirit", border="cells", width=80, align="c")
-            table.add_row("Strength: " + str(self.caller.db.attributes['Strength']), "Wits: " + str(self.caller.db.attributes['Wits']), "Presence: " + str(self.caller.db.attributes['Presence']))
-            table.add_row("Dexterity: " + str(self.caller.db.attributes['Dexterity']), "Perception: " + str(self.caller.db.attributes['Perception']), "Will: " + str(self.caller.db.attributes['Will']))
-            table.add_row("Endurance: " + str(self.caller.db.attributes['Endurance']), "Tech: " + str(self.caller.db.attributes['Tech']), "Faith: " + str(self.caller.db.attributes['Faith']))
-            self.caller.msg(table)
-            self.caller.msg(pad(" Skills ", width=80, align="c", fillchar="="))
-            i = 0
+        self.caller.msg(pad("Demographics", width=80))
+        self.caller.msg(pad("Name: " + self.caller.key, width=40, align="l") + pad("Archetype: " + self.caller.db.archetype, width=40, align="r"))
+        self.caller.msg(pad("Faction: " + self.caller.db.house, width=40, align="l") + pad("Rank: " + self.caller.db.benefices['Rank'], width=40, align="r"))
+        self.caller.msg(pad("Firebirds: " + str(self.caller.db.firebirds), width=40, align="l") + pad("Assets: " + str(self.caller.db.assets), width=40, align="r"))
+        self.caller.msg(pad("Attributes", width=80, align="c", fillchar="="))
+        table = evtable.EvTable("Body", "Mind", "Spirit", border="cells", width=80, align="c")
+        table.add_row("Strength: " + str(self.caller.db.attributes['Strength']), "Wits: " + str(self.caller.db.attributes['Wits']), "Presence: " + str(self.caller.db.attributes['Presence']))
+        table.add_row("Dexterity: " + str(self.caller.db.attributes['Dexterity']), "Perception: " + str(self.caller.db.attributes['Perception']), "Will: " + str(self.caller.db.attributes['Will']))
+        table.add_row("Endurance: " + str(self.caller.db.attributes['Endurance']), "Tech: " + str(self.caller.db.attributes['Tech']), "Faith: " + str(self.caller.db.attributes['Faith']))
+        self.caller.msg(table)
+        self.caller.msg(pad(" Skills ", width=80, align="c", fillchar="="))
+        i = 0
+        temp = ''
+        for x in self.caller.db.skills.keys():
+            if 'Lore' not in x:
+                temp += "{0:^16}: {1:>6} ".format(crop(x, 16, '...'), self.caller.db.skills[x])
+                if i == 2:
+                    self.caller.msg(temp)
+                    temp = ''
+                    i=0
+                else:   
+                    i+=1
+        self.caller.msg(pad(" Lores ", width=80, align="c", fillchar="="))
+        i = 0
+        temp = ''
+        for x in self.caller.db.skills.keys():
+            if 'Lore' in x:
+                print x
+                print x.split('.')
+                temp += "{0:^16}: {1:>6}".format(crop(x.split('.')[1],16,'...'), self.caller.db.skills[x])
+                if i == 1:
+                    self.caller.msg(temp)
+                    temp = ''
+                    i = 0
+                else:
+                    i += 1
+        if len(self.caller.db.occult):
+            self.caller.msg(pad(" Occult ", width=80, align="c", fillchar="="))
+            for x in self.caller.db.occult.keys():
+                self.caller.msg(wrap(x + ": " + self.caller.db.occult[x], width=80, indent=3))
+        if len(self.caller.db.actions):
+            self.caller.msg(pad(" Fighting Styles ", width=80, align="c", fillchar="="))
+            for x in self.caller.db.actions.keys():
+                self.caller.msg(wrap(x + ": " + self.caller.db.actions[x], width=80, indent=3))
+        if len(self.caller.db.languages):
+            self.caller.msg(pad(" Languages ", width=80, align="c", fillchar="="))
             temp = ''
-            for x in self.caller.db.skills.keys():
-                if 'Lore' not in x:
-                    temp += "{0:^16}: {1:>6} ".format(crop(x, 16, '...'), self.caller.db.skills[x])
-                    if i == 2:
-                        self.caller.msg(temp)
-                        temp = ''
-                        i=0
-                    else:   
-                        i+=1
-            self.caller.msg(pad(" Lores ", width=80, align="c", fillchar="="))
             i = 0
-            temp = ''
-            for x in self.caller.db.skills.keys():
-                if 'Lore' in x:
-                    print x
-                    print x.split('.')
-                    temp += "{0:^16}: {1:>6}".format(crop(x.split('.')[1],16,'...'), self.caller.db.skills[x])
-                    if i == 1:
-                        self.caller.msg(temp)
-                        temp = ''
-                        i = 0
-                    else:
-                        i += 1
-            if len(self.caller.db.occult):
-                self.caller.msg(pad(" Occult ", width=80, align="c", fillchar="="))
-                for x in self.caller.db.occult.keys():
-                    self.caller.msg(wrap(x + ": " + self.caller.db.occult[x], width=80, indent=3))
-            if len(self.caller.db.actions):
-                self.caller.msg(pad(" Fighting Styles ", width=80, align="c", fillchar="="))
-                for x in self.caller.db.actions.keys():
-                    self.caller.msg(wrap(x + ": " + self.caller.db.actions[x], width=80, indent=3))
-            if len(self.caller.db.languages):
-                self.caller.msg(pad(" Languages ", width=80, align="c", fillchar="="))
-                temp = ''
-                i = 0
-                for x in self.caller.db.languages:
-                    temp += "{0:^16}".format(crop(x,16,'...'))
-                    if i == 2:
-                        self.caller.msg(temp)
-                        i = 0
-                    else:
-                        i += 1
-            
+            for x in self.caller.db.languages:
+                temp += "{0:^16}".format(crop(x,16,'...'))
+                if i == 2:
+                    self.caller.msg(temp)
+                    i = 0
+                else:
+                    i += 1
+        
             
 class ChargenStart(default_cmds.MuxCommand):
     """
